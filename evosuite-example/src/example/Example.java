8fc0e1968e47385566dc6e6d484b59f628d50117
Yun Lin <llmhyy@gmail.com>
2022-04-18 21:56:50 +0800

    ways to terminate the thread pool executor

--- evosuite-example/src/example/Example.java
+++ evosuite-example/src/example/Example.java
@@ -3,29 +3,37 @@ package example;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.LockSupport;
 
 import org.evosuite.Properties;
+import org.evosuite.Properties.StatisticsBackend;
 //import org.evosuite.Properties.StatisticsBackend;
 import org.evosuite.result.BranchInfo;
+import org.evosuite.utils.LoggingUtils;
 
 import evosuite.shell.EvoTestResult;
 import evosuite.shell.experiment.SFConfiguration;
 
 public class Example {
 
+	public static void main(String[] args) throws Exception {
+		Example example = new Example();
+		example.run();
+	}
+	
 	/**
 	 * We need to support JDK1.8 as the Java Home, 
 	 * one concern is that the threads will not be fully killed after getting the test results,
 	 * the user may need to kill the main process in an explicit way.
 	 */
-	public static void main(String[] args) throws Exception {
+	public void run() throws Exception {
 		
 		
 		/**
 		 * The following code is used for debugging
 		 */
-//		Properties.CLIENT_ON_THREAD = true;
-//		Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
+		Properties.CLIENT_ON_THREAD = true;
+		Properties.STATISTICS_BACKEND = StatisticsBackend.DEBUG;
 
 		Properties.ENABLE_BRANCH_ENHANCEMENT = false;
 		Properties.ADOPT_SMART_MUTATION = false;
@@ -50,7 +58,7 @@ public class Example {
 		 */
 		List<EvoTestResult> results = new ArrayList<EvoTestResult>();
 		int repeatTime = 1;
-		int budget = 10;
+		int budget = 5;
 		Long seed = null;
 		
 		String fitnessApproach = "branch";
@@ -70,9 +78,48 @@ public class Example {
 				
 			}
 		}
-
 		
+		System.out.println("test done");
 		System.currentTimeMillis();
+
+		Thread[] threadArray = new Thread[Thread.activeCount() + 2];
+		Thread.enumerate(threadArray);
+		
+		for (Thread t : threadArray) {
+			if(t == null){
+				continue;
+			}
+			
+			if(t.getName().equals("main")) {
+				continue;
+			}
+
+			kill(t);
+		}
+		
+//		SandboxFromJUnitTest.getInstance().
+		
+	}
+
+	private void kill(Thread t) throws InterruptedException {
+		while (t.isAlive()) {
+			t.interrupt();
+			Object obj = LockSupport.getBlocker(t);
+			if(obj != null) {
+				LockSupport.unpark(t);		
+				t.interrupt();
+				
+				//TODO get the ThreadPoolExecutor via thread.target..., then shutdown, 
+				//TODO it should work
+				
+			}
+			
+			Thread.sleep(100);
+			
+			if(t.isDaemon()) {
+				break;
+			}
+		}
 	}
 
 }
---
+++ evosuite-example/src/example/TestRun.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.junit.Test;
+
+public class TestRun {
+
+	@Test
+	public void test() throws Exception {
+		Example example = new Example();
+		example.run();
+	}
+
+}
